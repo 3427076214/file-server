760092b90131e18db69ea998238d7f6c4c89d2bd
gongchengxiang <user5056@163.com>
2022-01-20 09:17:27 +0800

    test

--- BuildLinux.cmd
+++ BuildLinux.cmd
@@ -1,7 +1,7 @@
 set GOOS=linux
 set GOARCH=amd64
 set CGO_ENABLED=0 
-go build -o file-server
+go build -o ./bin/file-server
 
 if %ERRORLEVEL% gtr 0 (
     pause
--- start-server.sh
+++ start-server.sh
@@ -1,8 +1,9 @@
 
-read -t 30 -p "Please input authtoken: " myauthtoken
+read -t 300 -p "Please input authtoken: " myauthtoken
 echo -e "\n"
 echo "authtoken is $myauthtoken"
 
+cd ./bin
 chmod a+x natapp
 chmod a+x file-server
 
